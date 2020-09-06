160a7eae93acc82e10957ece9ebd9cc89f772ecd
Elton Leite <borelanjo@gmail.com>
2020-09-06 17:21:53 -0300

    :sparkles: Adiciona mensagens do Twitter para um Kafka Producer

--- src/main/java/com/borelanjo/TwitterProducerApplication.java
+++ src/main/java/com/borelanjo/TwitterProducerApplication.java
@@ -1,11 +1,17 @@
 package com.borelanjo;
 
-import com.borelanjo.application.configuration.property.TwitterProperty;
 import com.borelanjo.infrastructure.client.TwitterClient;
+import com.borelanjo.infrastructure.producer.KafkaProducerCreator;
+import com.google.common.collect.Lists;
 import com.twitter.hbc.core.Client;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -19,11 +25,26 @@ public class TwitterProducerApplication {
     }
 
     public void run() {
-        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100_000);
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1_000);
+        List<String> terms = Lists.newArrayList("Deus", "Igreja Batista", "Adorar");
 
-        Client client = new TwitterClient().create(msgQueue);
+        Client client = new TwitterClient().create(msgQueue, terms);
 
         client.connect();
+
+        KafkaProducer<String, String> producer = new KafkaProducerCreator().create();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Stopping application");
+
+            logger.info("shutting down client from twitter...");
+            client.stop();
+
+            logger.info("closing producer");
+            producer.close();
+
+            logger.info("bye bye!");
+        }));
 
         while (!client.isDone()) {
             String msg = null;
@@ -35,10 +56,18 @@ public class TwitterProducerApplication {
             }
 
             if(msg != null){
-                logger.info(msg);
+                producer.send(new ProducerRecord<>("twitter_tweets", null, msg), new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                        if(e != null){
+                            logger.error("Something bad happened", e);
+                        }
+                    }
+                });
             }
         }
-        
+
+        logger.info("The end");
         client.stop();
     }
 
