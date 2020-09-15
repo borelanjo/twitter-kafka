72c7de75524f45360c2f723b49b34b7a7abf375f
Elton Leite <borelanjo@gmail.com>
2020-09-14 23:13:41 -0300

    :sparkles: Ativa configuração de idempotência no produtor do Kafka

--- src/main/java/com/borelanjo/infrastructure/producer/KafkaProducerCreator.java
+++ src/main/java/com/borelanjo/infrastructure/producer/KafkaProducerCreator.java
@@ -1,33 +1,18 @@
 package com.borelanjo.infrastructure.producer;
 
 import com.borelanjo.application.configuration.property.KafkaProperty;
-import com.twitter.hbc.ClientBuilder;
-import com.twitter.hbc.core.Client;
-import com.twitter.hbc.core.Constants;
-import com.twitter.hbc.core.Hosts;
-import com.twitter.hbc.core.HttpHosts;
-import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
-import com.twitter.hbc.core.processor.StringDelimitedProcessor;
-import com.twitter.hbc.httpclient.auth.Authentication;
-import com.twitter.hbc.httpclient.auth.OAuth1;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.BlockingQueue;
 
 public class KafkaProducerCreator {
 
     private KafkaProperty kafkaProperty = new KafkaProperty();
 
     public KafkaProducer<String, String> create() {
-        KafkaProducer<String, String> producer = new KafkaProducer<>(getProperties());
-        return producer;
+        return new KafkaProducer<>(getProperties());
     }
 
     private Properties getProperties() {
@@ -35,6 +20,12 @@ public class KafkaProducerCreator {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperty.getBootstrapServers());
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
         return properties;
     }
 }
