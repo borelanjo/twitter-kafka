26ec7bb7989fb35772ce03cd15a3628d232e8903
Elton Leite <borelanjo@gmail.com>
2020-09-15 21:53:47 -0300

    :sparkles: Transforma termos em lista global

--- src/main/java/com/borelanjo/TwitterProducerApplication.java
+++ src/main/java/com/borelanjo/TwitterProducerApplication.java
@@ -1,13 +1,11 @@
 package com.borelanjo;
 
 import com.borelanjo.infrastructure.client.TwitterClient;
-import com.borelanjo.infrastructure.producer.KafkaProducerCreator;
+import com.borelanjo.infrastructure.factory.KafkaProducerFactory;
 import com.google.common.collect.Lists;
 import com.twitter.hbc.core.Client;
-import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,22 +15,24 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 public class TwitterProducerApplication {
-    
-    Logger logger = LoggerFactory.getLogger("TwitterProducerApplication");
-    
+
+    private Logger logger = LoggerFactory.getLogger("TwitterProducerApplication");
+
+    private List<String> terms = Lists.newArrayList("covid");
+
     public static void main(String[] args) {
         new TwitterProducerApplication().run();
     }
 
     public void run() {
-        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1_000);
-        List<String> terms = Lists.newArrayList("Deus", "Igreja Batista", "Adorar");
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<>(1_000);
+
 
         Client client = new TwitterClient().create(msgQueue, terms);
 
         client.connect();
 
-        KafkaProducer<String, String> producer = new KafkaProducerCreator().create();
+        KafkaProducer<String, String> producer = new KafkaProducerFactory().create();
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             logger.info("Stopping application");
@@ -46,29 +46,30 @@ public class TwitterProducerApplication {
             logger.info("bye bye!");
         }));
 
-        while (!client.isDone()) {
-            String msg = null;
-            try {
-                msg = msgQueue.poll(5, TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                client.stop();
+        try {
+            while (!client.isDone()) {
+                String msg = msgQueue.poll(5, TimeUnit.SECONDS);
+                if (msg != null) {
+                    producer.send(new ProducerRecord<>("twitter_tweets", null, msg),
+                            (recordMetadata, e) -> {
+                                if (e != null) {
+                                    logger.error("Something bad happened", e);
+                                }
+                            });
+                }
             }
-
-            if(msg != null){
-                producer.send(new ProducerRecord<>("twitter_tweets", null, msg), new Callback() {
-                    @Override
-                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                        if(e != null){
-                            logger.error("Something bad happened", e);
-                        }
-                    }
-                });
-            }
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage(), e);
+            client.stop();
+            producer.close();
+            Thread.currentThread().interrupt();
+        } finally {
+            client.stop();
+            producer.close();
         }
 
         logger.info("The end");
-        client.stop();
+
     }
 
 }
