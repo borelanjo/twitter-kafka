ef644a96c4af96406fc3dd143d7b5dc1aba624b2
Elton Leite <borelanjo@gmail.com>
2020-09-15 21:29:53 -0300

    :sparkles: Adiciona propriedades para compressão e taxa de transferência de Producer

--- src/main/java/com/borelanjo/infrastructure/factory/KafkaProducerFactory.java
+++ src/main/java/com/borelanjo/infrastructure/factory/KafkaProducerFactory.java
@@ -1,4 +1,4 @@
-package com.borelanjo.infrastructure.producer;
+package com.borelanjo.infrastructure.factory;
 
 import com.borelanjo.application.configuration.property.KafkaProperty;
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -7,7 +7,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
-public class KafkaProducerCreator {
+public class KafkaProducerFactory {
 
     private KafkaProperty kafkaProperty = new KafkaProperty();
 
@@ -21,11 +21,23 @@ public class KafkaProducerCreator {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        addSafeProperties(properties);
+
+        addThroughputProperties(properties);
+
+        return properties;
+    }
+
+    private void addThroughputProperties(Properties properties) {
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024));
+    }
+
+    private void addSafeProperties(Properties properties) {
         properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
         properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
         properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
         properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
-
-        return properties;
     }
 }
